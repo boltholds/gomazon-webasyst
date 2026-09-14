c6b32144bce37365c14a6ebefb87256a4d29ae86
bolthold <49822014+boltholds@users.noreply.github.com>
2026-09-14 20:25:20 +0300

    feat: add unit of work port

---
+++ src/gomazon_webasyst/application/ports/unit_of_work.py
@@ -0,0 +1,25 @@
+from types import TracebackType
+from typing import Protocol, Self
+
+from .contacts import ContactRepository
+
+
+class UnitOfWork(Protocol):
+    contacts: ContactRepository
+
+    async def __aenter__(self) -> Self: ...
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc: BaseException | None,
+        tb: TracebackType | None,
+    ) -> None: ...
+
+    async def commit(self) -> None: ...
+
+    async def rollback(self) -> None: ...
+
+
+class UnitOfWorkFactory(Protocol):
+    def __call__(self) -> UnitOfWork: ...
