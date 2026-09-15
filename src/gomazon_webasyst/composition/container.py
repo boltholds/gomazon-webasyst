a940d571191b07947e02d1efbf3fca4dd2a67563
bolthold <49822014+boltholds@users.noreply.github.com>
2026-09-15 17:51:53 +0300

    feat: expose access control use cases in container

--- src/gomazon_webasyst/composition/container.py
+++ src/gomazon_webasyst/composition/container.py
@@ -2,6 +2,25 @@ from dataclasses import dataclass
 
 from sqlalchemy.ext.asyncio import AsyncEngine
 
+from gomazon_webasyst.application.access_control import (
+    AddGroupMember,
+    AssignRight,
+    CreateGroup,
+    DeleteGroup,
+    GetAppAccess,
+    GetEffectiveRight,
+    GetGroup,
+    GetRightsSnapshot,
+    ListGroupMembers,
+    ListGroups,
+    ListUserGroups,
+    RemoveGroupMember,
+    ReplaceGroupMembers,
+    RevokeRight,
+    SetAppAccess,
+    SetGlobalAdminAccess,
+    UpdateGroup,
+)
 from gomazon_webasyst.application.auth import (
     AuthenticateBackendPassword,
     LogoutBackendSession,
@@ -13,6 +32,7 @@ from gomazon_webasyst.application.persistent_login import (
     RestoreBackendSessionFromPersistentCredential,
     RevokePersistentCredential,
 )
+from gomazon_webasyst.composition.access_control import create_access_control_use_cases
 from gomazon_webasyst.composition.auth import create_auth_use_cases
 from gomazon_webasyst.composition.settings import Settings
 from gomazon_webasyst.infrastructure.persistence.sqlalchemy.factory import (
@@ -35,6 +55,23 @@ class Container:
     issue_persistent_credential: IssuePersistentCredential
     restore_backend_session_from_persistent_credential: RestoreBackendSessionFromPersistentCredential
     revoke_persistent_credential: RevokePersistentCredential
+    get_group: GetGroup
+    list_groups: ListGroups
+    list_user_groups: ListUserGroups
+    list_group_members: ListGroupMembers
+    get_effective_right: GetEffectiveRight
+    get_app_access: GetAppAccess
+    get_rights_snapshot: GetRightsSnapshot
+    create_group: CreateGroup
+    update_group: UpdateGroup
+    delete_group: DeleteGroup
+    add_group_member: AddGroupMember
+    remove_group_member: RemoveGroupMember
+    replace_group_members: ReplaceGroupMembers
+    assign_right: AssignRight
+    revoke_right: RevokeRight
+    set_app_access: SetAppAccess
+    set_global_admin_access: SetGlobalAdminAccess
 
     async def close(self) -> None:
         await self.engine.dispose()
@@ -43,7 +80,9 @@ class Container:
 def create_container(settings: Settings) -> Container:
     engine = create_engine(settings)
     uow_factory = create_uow_factory(engine)
-    auth = create_auth_use_cases(create_session_factory(engine))
+    session_factory = create_session_factory(engine)
+    auth = create_auth_use_cases(session_factory)
+    access = create_access_control_use_cases(session_factory)
     return Container(
         settings=settings,
         engine=engine,
@@ -58,4 +97,21 @@ def create_container(settings: Settings) -> Container:
             auth.restore_backend_session_from_persistent_credential
         ),
         revoke_persistent_credential=auth.revoke_persistent_credential,
+        get_group=access.get_group,
+        list_groups=access.list_groups,
+        list_user_groups=access.list_user_groups,
+        list_group_members=access.list_group_members,
+        get_effective_right=access.get_effective_right,
+        get_app_access=access.get_app_access,
+        get_rights_snapshot=access.get_rights_snapshot,
+        create_group=access.create_group,
+        update_group=access.update_group,
+        delete_group=access.delete_group,
+        add_group_member=access.add_group_member,
+        remove_group_member=access.remove_group_member,
+        replace_group_members=access.replace_group_members,
+        assign_right=access.assign_right,
+        revoke_right=access.revoke_right,
+        set_app_access=access.set_app_access,
+        set_global_admin_access=access.set_global_admin_access,
     )
